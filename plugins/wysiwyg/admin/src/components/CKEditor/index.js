404727f25b42d730ee249599c5872223da8de72d
huug buug <yondooo61@gmail.com>
2021-04-19 09:49:07 +0800

    dd

--- plugins/wysiwyg/admin/src/components/CKEditor/index.js
+++ plugins/wysiwyg/admin/src/components/CKEditor/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import ClassicEditor from './build2/ckeditor';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
@@ -14,75 +14,94 @@ const Wrapper = styled.div`
 `;
 
 const configuration = {
-    // Configure the available styles.
+  image: {
     styles: [
-        'alignLeft', 'alignCenter', 'alignRight'
+      'alignLeft', 'alignCenter', 'alignRight'
+    ],
+    resizeOptions: [
+      {
+        name: 'resizeImage:original',
+        label: 'Original',
+        value: null
+      },
+      {
+        name: 'resizeImage:25',
+        label: '25%',
+        value: '25'
+      },
+      {
+        name: 'resizeImage:50',
+        label: '50%',
+        value: '50'
+      },
+      {
+        name: 'resizeImage:75',
+        label: '75%',
+        value: '75'
+      },
+      {
+        name: 'resizeImage:100',
+        label: '100%',
+        value: '100'
+      }
     ],
 
-    // Configure the available image resize options.
-    resizeOptions: [
-        {
-            name: 'resizeImage:original',
-            label: 'Original',
-            value: null
-        },
-        {
-            name: 'resizeImage:50',
-            label: '50%',
-            value: '50'
-        },
-        {
-            name: 'resizeImage:75',
-            label: '75%',
-            value: '75'
-        }
-    ],
     toolbar: [
-        'heading',
-        '|',
-        'bold',
-        'italic',
-        'link',
-        'bulletedList',
-        'numberedList',
-        '|',
-        'indent',
-        'outdent',
-        '|',
-        'blockQuote',
-        'insertTable',
-        'mediaEmbed',
-        'undo',
-        'redo',
-    ],
+      'imageStyle:alignLeft', 'imageStyle:alignCenter', 'imageStyle:alignRight',
+      '|',
+      'resizeImage',
+      '|',
+      'imageTextAlternative'
+    ]
+  },
+  toolbar: [
+    'heading',
+    '|',
+    'alignment',
+    'fontColor',
+    'fontBackgroundColor',
+    'fontSize',
+    '|',
+    'bold',
+    'italic',
+    'link',
+    'bulletedList',
+    'numberedList',
+    '|',
+    'indent',
+    'outdent',
+    '|',
+    'blockQuote',
+    'insertTable',
+    'mediaEmbed',
+    'undo',
+    'redo',
+    '|',
+    'wordCount',
+    ''
+  ],
 };
 
 const Editor = ({ onChange, name, value }) => {
-    return (
-        <Wrapper>
-            <CKEditor
-                editor={ClassicEditor}
-                config={configuration}
-                data={value}
-                onReady={(editor) => {
-                    if (value) {
-                       editor.setData(value);
-                    }
-                }}
-                onChange={(event, editor) => {
-                    const data = editor.getData();
-                    onChange({ target: { name, value: data } });
-                    
-                }}
-            />
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <CKEditor
+        editor={ClassicEditor}
+        config={configuration}
+        data={value}
+        onChange={(event, editor) => {
+          const data = editor.getData();
+          onChange({ target: { name, value: data } });
+        }}
+      />
+    </Wrapper>
+  );
 };
 
 Editor.propTypes = {
-    onChange: PropTypes.func.isRequired,
-    name: PropTypes.string.isRequired,
-    value: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string,
 };
 
 export default Editor;
